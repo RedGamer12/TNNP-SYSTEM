10410540e3c72e6bbe30ef074c5abd91508de4bf
RedGamer12 <73015566+RedGamer12@users.noreply.github.com>
2025-03-19 08:54:47 +0700

    Update shared.lua

--- env/shared.lua
+++ env/shared.lua
@@ -3,6 +3,7 @@ local SharedModule = {}
 SharedModule.__urlcache = SharedModule.__urlcache or {}
 SharedModule.threads = SharedModule.threads or {}
 SharedModule.callbacks = {}
+SharedModule.isRunning = false
 
 if SharedModule._unload then
     pcall(SharedModule._unload)
@@ -16,31 +17,42 @@ function SharedModule._unload()
     end
     SharedModule.threads = {}
 
-    Fluent:Destroy()
+    if _G.Library then
+        _G.Library:Destroy()
+    end
 
     for _, callback in ipairs(SharedModule.callbacks) do
         task.spawn(callback)
     end
     SharedModule.callbacks = {}
+    SharedModule.isRunning = false
 end
 
-function SharedModule.init(threads)
-    SharedModule._unload()
+function SharedModule.init(threads, enable, threadName)
+    if not enable then
+        SharedModule._unload()
+        return
+    end
+    
+    if SharedModule.isRunning then return end
+    SharedModule.isRunning = true
     
     for _, threadInfo in ipairs(threads) do
-        local thread = task.spawn(function()
-            print("Thread started:", threadInfo.name)
-            while true do
-                threadInfo.func()
-                task.wait()
-            end
-        end)
+        if not threadName or threadInfo.name == threadName then
+            local thread = task.spawn(function()
+                print("Thread started:", threadInfo.name)
+                while SharedModule.isRunning do
+                    threadInfo.func()
+                    task.wait()
+                end
+            end)
 
-        table.insert(SharedModule.threads, thread)
-        table.insert(SharedModule.callbacks, function()
-            print("Canceling thread:", threadInfo.name)
-            pcall(task.cancel, thread)
-        end)
+            table.insert(SharedModule.threads, thread)
+            table.insert(SharedModule.callbacks, function()
+                print("Canceling thread:", threadInfo.name)
+                pcall(task.cancel, thread)
+            end)
+        end
     end
 end
 
